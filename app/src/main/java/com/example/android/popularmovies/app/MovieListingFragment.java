9ff5e90c49de4370fd49f0f911090a837472f50b
Zach Kost-Smith <zachkostsmith@gmail.com>
2016-04-02 21:32:45 -0500

    Change MovieListingFragment to query TMDB
    This also uses the spinner value to determine the ordering of the data
    and it implements the listeners to update if the user makes a change to
    the spinner value.

--- app/src/main/java/com/example/android/popularmovies/app/MovieListingFragment.java
+++ app/src/main/java/com/example/android/popularmovies/app/MovieListingFragment.java
@@ -79,7 +79,8 @@ public class MovieListingFragment extends Fragment {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("94043");
+            Spinner spinner = (Spinner) getActivity().findViewById(R.id.sort_spinner);
+            weatherTask.execute(String.valueOf(spinner.getSelectedItemPosition()));
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -111,7 +112,7 @@ public class MovieListingFragment extends Fragment {
                 "Movie title",
                 "Movie title",
                 "Movie title"};
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+        List<String> movieTitles = new ArrayList<String>(Arrays.asList(data));
 
         // Now that we have some dummy forecast data, create an ArrayAdapter.
         // The ArrayAdapter will take data from a source (like our dummy forecast) and
@@ -121,7 +122,7 @@ public class MovieListingFragment extends Fragment {
                         getActivity(), // The current context (this activity)
                         R.layout.list_item_forecast, // The name of the layout ID.
                         R.id.list_item_forecast_textview, // The ID of the textview to populate.
-                        weekForecast);
+                        movieTitles);
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
@@ -133,6 +134,23 @@ public class MovieListingFragment extends Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        FetchWeatherTask weatherTask = new FetchWeatherTask();
+        weatherTask.execute(String.valueOf(spinner.getSelectedItemPosition()));
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                FetchWeatherTask weatherTask = new FetchWeatherTask();
+                weatherTask.execute(String.valueOf(i));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
 
         // Get a reference to the ListView, and attach this adapter to it.
         GridView gridView = (GridView) rootView.findViewById(R.id.grid_view_forecast);
@@ -185,67 +203,17 @@ public class MovieListingFragment extends Fragment {
          * Fortunately parsing is easy:  constructor takes the JSON string and converts it
          * into an Object hierarchy for us.
          */
-        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+        private String[] getWeatherDataFromJson(String forecastJsonStr)
                 throws JSONException {
 
-            // These are the names of the JSON objects that need to be extracted.
-            final String OWM_LIST = "list";
-            final String OWM_WEATHER = "weather";
-            final String OWM_TEMPERATURE = "temp";
-            final String OWM_MAX = "max";
-            final String OWM_MIN = "min";
-            final String OWM_DESCRIPTION = "main";
-
             JSONObject forecastJson = new JSONObject(forecastJsonStr);
-            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+            JSONArray results = forecastJson.getJSONArray("results");
 
-            // OWM returns daily forecasts based upon the local time of the city that is being
-            // asked for, which means that we need to know the GMT offset to translate this data
-            // properly.
+            String[] resultStrs = new String[results.length()];
+            for(int i = 0; i < results.length(); i++) {
 
-            // Since this data is also sent in-order and the first day is always the
-            // current day, we're going to take advantage of that to get a nice
-            // normalized UTC date for all of our weather.
-
-            Time dayTime = new Time();
-            dayTime.setToNow();
-
-            // we start at the day returned by local time. Otherwise this is a mess.
-            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
-
-            // now we work exclusively in UTC
-            dayTime = new Time();
-
-            String[] resultStrs = new String[numDays];
-            for(int i = 0; i < weatherArray.length(); i++) {
-                // For now, using the format "Day, description, hi/low"
-                String day;
-                String description;
-                String highAndLow;
-
-                // Get the JSON object representing the day
-                JSONObject dayForecast = weatherArray.getJSONObject(i);
-
-                // The date/time is returned as a long.  We need to convert that
-                // into something human-readable, since most people won't read "1400356800" as
-                // "this saturday".
-                long dateTime;
-                // Cheating to convert this to UTC time, which is what we want anyhow
-                dateTime = dayTime.setJulianDay(julianStartDay+i);
-                day = getReadableDateString(dateTime);
-
-                // description is in a child array called "weather", which is 1 element long.
-                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
-                description = weatherObject.getString(OWM_DESCRIPTION);
-
-                // Temperatures are in a child object called "temp".  Try not to name variables
-                // "temp" when working with temperature.  It confuses everybody.
-                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
-                double high = temperatureObject.getDouble(OWM_MAX);
-                double low = temperatureObject.getDouble(OWM_MIN);
-
-                highAndLow = formatHighLows(high, low);
-                resultStrs[i] = day + " - " + description + " - " + highAndLow;
+                JSONObject movie = results.getJSONObject(i);
+                resultStrs[i] = movie.getString("title");
             }
 
             for (String s : resultStrs) {
@@ -270,27 +238,27 @@ public class MovieListingFragment extends Fragment {
             // Will contain the raw JSON response as a string.
             String forecastJsonStr = null;
 
-            String format = "json";
-            String units = "metric";
-            int numDays = 7;
+            String sortOption;
+            switch (params[0])
+            {
+                case "0":
+                    sortOption = "popular";
+                    break;
+                case "1":
+                    sortOption = "top_rated";
+                    break;
+                default:
+                    Log.e("MovieListingFragment", "Invalid sort option");
+                    return null;
+            }
 
             try {
-                // Construct the URL for the OpenWeatherMap query
-                // Possible parameters are avaiable at OWM's forecast API page, at
-                // http://openweathermap.org/API#forecast
                 final String FORECAST_BASE_URL =
-                        "http://api.openweathermap.org/data/2.5/forecast/daily?";
-                final String QUERY_PARAM = "q";
-                final String FORMAT_PARAM = "mode";
-                final String UNITS_PARAM = "units";
-                final String DAYS_PARAM = "cnt";
+                    "https://api.themoviedb.org/3/movie/";
 
-                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                        .appendQueryParameter(QUERY_PARAM, params[0])
-                        .appendQueryParameter(FORMAT_PARAM, format)
-                        .appendQueryParameter(UNITS_PARAM, units)
-                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-                        .build();
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon().appendPath(sortOption)
+                    .appendQueryParameter("api_key", getString(R.string.THE_MOVIE_DB_API_TOKEN))
+                    .build();
 
                 URL url = new URL(builtUri.toString());
 
@@ -344,7 +312,7 @@ public class MovieListingFragment extends Fragment {
             }
 
             try {
-                return getWeatherDataFromJson(forecastJsonStr, numDays);
+                return getWeatherDataFromJson(forecastJsonStr);
             } catch (JSONException e) {
                 Log.e(LOG_TAG, e.getMessage(), e);
                 e.printStackTrace();
